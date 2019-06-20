b5afd2fbeb7d07b396e027692ba1b397b1008c1e
RoyKio <1759761439@qq.com>
2019-06-20 14:15:00 +0800

    Update main_desktop.dart

--- lib/main_desktop.dart
+++ lib/main_desktop.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:opencoolapk/main.dart' show OpenCoolapk;
 import 'package:flutter/foundation.dart' show debugDefaultTargetPlatformOverride;
 
+// hover使用这个文件作为入口
+// hover: https://github.com/go-flutter-desktop/hover
+
 void main() {
   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   runApp(OpenCoolapk());
